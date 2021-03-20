c4b4f53310f00262f460ae03287cc1714f72cf0c
atakanbal <atakannbal@gmail.com>
2021-03-20 18:57:53 +0300

    feat: get initial data on render

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import * as React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import {handleInitialData} from '../actions/shared'
 
 export default function App() {
+  const dispatch = useDispatch()
+  const loading = useSelector((state) => state.authedUser === null);
+
+  React.useEffect(() => {
+    dispatch(handleInitialData())
+  },[dispatch, handleInitialData])
+
   const store = useSelector((store) => store)
   console.log('Store', store)
   
